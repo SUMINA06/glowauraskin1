c8e4590f9c0385f0393a5ce8194b89c727ae36a8
Sumina <upretisumee07@gmail.com>
2026-08-27 18:29:22 +0545

    Update backend changes

--- backend/server.js
+++ backend/server.js
@@ -28,10 +28,7 @@ const app = express();
 // ==============================
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://glowauraskin.up.railway.app",
-    ],
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
