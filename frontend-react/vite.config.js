74efce3af194ec9a42e4a710e10aecd9b96bef4f
Sumina Upreti <upretisumee07@gmail.com>
2026-08-28 08:50:57 +0545

    Configure preview settings in Vite
    Add preview configuration with allowed hosts for deployment.

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 5173,
+
     proxy: {
       "/api": {
         target: "http://localhost:3000",
@@ -16,5 +18,9 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+
+  preview: {
+    allowedHosts: ["glowskinaura.up.railway.app"],
   },
 })
